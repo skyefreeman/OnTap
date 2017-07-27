bc6a462453c5dd8044fbfd2453b3b9d35540088b
Skye Freeman <skyefreeman@icloud.com>
2017-07-27 15:49:40 -0400

    improved example project to include on screen visual feedback

--- Example/OnTap/ViewController.swift
+++ Example/OnTap/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import OnTap
 
 extension UIView {
+    
     var x: CGFloat { return frame.origin.x }
     var y: CGFloat { return frame.origin.y }
     
@@ -24,6 +25,17 @@ extension UIView {
     }
 }
 
+extension UILabel {
+    static func standardLabel(text: String, textColor: UIColor, backgroundColor: UIColor = UIColor.clear) -> UILabel {
+        let label = UILabel()
+        label.text = text
+        label.textColor = textColor
+        label.textAlignment = .center
+        label.backgroundColor = backgroundColor
+        return label
+    }
+}
+
 class ViewController: UIViewController {
     
     lazy var button: UIButton = {
@@ -31,47 +43,65 @@ class ViewController: UIViewController {
         button.backgroundColor = UIColor.blue
         button.setTitle(NSLocalizedString("UIButton", comment: ""), for: .normal)
         return button
-            .on(.touchDown) { print("touch down happend!!!") }
-            .on(.touchDownRepeat) { print("touch down repeated!!!") }
-            .on(.touchUpInside) { print("touch up inside happend!!!") }
-            .on(.touchUpOutside) { print("touch up outside happend!!!") }
+            .on(.touchDown) { [unowned self] in
+                self.buttonLabel.text = "touch down"
+            }
+            .on(.touchDownRepeat) { [unowned self] in
+                self.buttonLabel.text = "touch down repeat"
+            }
+            .on(.touchUpInside) { [unowned self] in
+                self.buttonLabel.text = "touch up inside"
+            }
+            .on(.touchUpOutside) { [unowned self] in
+                self.buttonLabel.text = "touch up outside"
+        }
     }()
 
     lazy var slider: UISlider = {
         return UISlider().on(.valueChanged) { [unowned self] in
-            print("slider new value: \(self.slider.value)")
+            self.sliderLabel.text = "\(self.slider.value)"
         }
     }()
 
-    lazy var switchView: UISwitch = {
+    lazy var switchView: UISwitch = { [unowned self] in
         return UISwitch().on(.valueChanged) { [unowned self] in
-            print("switch new value: \(self.switchView.isOn)")
+            self.switchLabel.text = self.switchView.isOn ? "On" : "Off"
         }
     }()
     
-    lazy var label: UILabel = {
-        let label = UILabel()
-        label.backgroundColor = UIColor.red
-        label.text = NSLocalizedString("Tap me!", comment: "")
-        label.textColor = UIColor.white
-        label.textAlignment = .center
-        return label
-            .on(.tap, touches: 1) { label.text = "tapped" }
-            .on(.leftSwipe, touches: 1) { label.text = "left" }
-            .on(.rightSwipe, touches: 1) { label.text = "right" }
-            .on(.upSwipe, touches: 1) { label.text = "up" }
-            .on(.downSwipe, touches: 1) { label.text = "down" }
+    lazy var gestureLabel: UILabel = {
+        return UILabel.standardLabel(
+            text: NSLocalizedString("Tap or swipe!", comment: ""),
+            textColor: UIColor.white,
+            backgroundColor: UIColor.red)
+            .on(.tap, touches: 1) { [unowned self] in self.gestureLabel.text = "tapped" }
+            .on(.leftSwipe, touches: 1) { [unowned self] in self.gestureLabel.text = "left swipe" }
+            .on(.rightSwipe, touches: 1) { [unowned self] in self.gestureLabel.text = "right swipe" }
+            .on(.upSwipe, touches: 1) { [unowned self] in self.gestureLabel.text = "upward swipe" }
+            .on(.downSwipe, touches: 1) { [unowned self] in self.gestureLabel.text = "downward swipe" }
+    }()
+    
+    lazy var sliderLabel: UILabel = { [unowned self] in
+        return UILabel.standardLabel(text: "\(self.slider.value)", textColor: UIColor.black)
+    }()
+    
+    lazy var switchLabel: UILabel = { [unowned self] in
+        return UILabel.standardLabel(text: "Off", textColor: UIColor.black)
+    }()
+    
+    lazy var buttonLabel: UILabel = {
+        return UILabel.standardLabel(text: "", textColor: UIColor.white)
     }()
     
     lazy var leftBarButtonItem: UIBarButtonItem = {
-        return UIBarButtonItem(title: NSLocalizedString("Left", comment: ""), style: .plain) {
-            print("left barButtonItem tapped!")
+        return UIBarButtonItem(title: NSLocalizedString("Left", comment: ""), style: .plain) { [unowned self] in
+            self.navigationItem.title = "left tapped"
         }
     }()
 
     lazy var rightBarButtonItem: UIBarButtonItem = {
-        return UIBarButtonItem(title: NSLocalizedString("Right", comment: ""), style: .plain) {
-            print("right barButtonItem tapped!")
+        return UIBarButtonItem(title: NSLocalizedString("Right", comment: ""), style: .plain) { [unowned self] in
+            self.navigationItem.title = "right tapped"
         }
     }()
     
@@ -80,18 +110,29 @@ class ViewController: UIViewController {
         
         let sharedSize = CGSize(width: view.width/2, height: view.height/2)
         button.frame = CGRect(x: 0, y: 0, width: sharedSize.width, height: sharedSize.height)
+        buttonLabel.frame = CGRect(x: 0, y: 0, width: sharedSize.width, height: sharedSize.height/2)
         slider.frame = CGRect(x: view.width/2, y: 0, width: sharedSize.width, height: sharedSize.height)
+        sliderLabel.frame = CGRect(x: view.width/2, y: 0, width: sharedSize.width, height: sharedSize.height/2)
         switchView.center = CGPoint(x: sharedSize.width/2, y: button.bottom + sharedSize.height/2)
-        label.frame = CGRect(x: view.width/2, y: slider.bottom, width: sharedSize.width, height: sharedSize.height)
+        switchLabel.frame = CGRect(x: 0, y: sharedSize.height, width: sharedSize.width, height: sharedSize.height/2)
+        gestureLabel.frame = CGRect(x: view.width/2, y: slider.bottom, width: sharedSize.width, height: sharedSize.height)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         navigationItem.leftBarButtonItem = leftBarButtonItem
         navigationItem.rightBarButtonItem = rightBarButtonItem
         
-        view.addSubviews(button, slider, switchView, label)
+        view.addSubviews(
+            button,
+            buttonLabel,
+            slider,
+            sliderLabel,
+            switchView,
+            switchLabel,
+            gestureLabel)
+        
         edgesForExtendedLayout = []
     }
 }
